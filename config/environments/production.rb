782e3368fe7ef95d637e537e9ed7233231e38140
Eric Sanders <easander@easander.com>
2012-11-01 03:16:07 -0500

    Reorganizing the precompiles so that it goes faster

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,9 @@ Recstache::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile << /^(?!rails_admin|jquery|partials)(?:.+\/)?(?!_)([^\/]*).s?css$/
+  config.assets.precompile << /^(?!rails_admin|jquery|partials)(?:.+\/)?(?!_)([^\/]*).js$/
+  config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
